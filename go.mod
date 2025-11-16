cd8913d4898814125ee21d9f745346275b8b4d73
R-Abinav <r.abinav1712@gmail.com>
2025-11-16 12:05:20 +0530

    chore: initial server setup

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/R-Abinav/GoFlux
+
+go 1.25.3
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	// "fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+);
+
+func main(){
+	godotenv.Load()
+	
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment or might be empty!")
+	}
+
+	//New router object
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
