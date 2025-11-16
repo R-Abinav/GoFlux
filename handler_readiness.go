e6a8c12abed9cd5ec2a84166d5aefb5e3552e4d7
R-Abinav <r.abinav1712@gmail.com>
2025-11-16 18:36:32 +0530

    chore: add a json message to health endpoint

--- handler_readiness.go
+++ handler_readiness.go
@@ -2,6 +2,14 @@ package main
 
 import "net/http"
 
+type HealthResponse struct{
+	Ok bool `json:"ok"`
+	Message string `json:"message"`
+}
+
 func handlerReadiness(w http.ResponseWriter, r *http.Request){
-	respondWithJSON(w, 200, struct{}{})
+	respondWithJSON(w, 200, HealthResponse{
+		Ok: true,
+		Message: "GoFlux API is up and running!",
+	});
 }
